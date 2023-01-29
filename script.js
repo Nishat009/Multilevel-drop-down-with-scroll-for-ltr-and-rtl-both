cd997193c420f628e592499252904112f8dac5b4
nishat009 <prantik.techvill@gmail.com>
2023-01-29 14:21:32 +0600

    multilevel dropdown

---
+++ script.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        }
+      }
+    }
+  }
+
+ 
+const scroller = document.querySelector(".nav-wrap");
+const firstDropDown = document.querySelectorAll(".first-dropdown");
+scroller.addEventListener("scroll", checkScroll);
+var s = getComputedStyle(document.getElementById('nav-wrap'))
+  function checkScroll() {
+      for (let i = 0; i < firstDropDown.length; i++) {
+        if(s.direction=="ltr"){
+          firstDropDown[i].style.transform ="translateX(-" + scroller.scrollLeft + "px)"  ;
+        }
+       else if(s.direction=="rtl"){
+        firstDropDown[i].style.transform ="translateX(+" + scroller.scrollLeft * (-1) + "px)"  ;
+    }
+}
+}
